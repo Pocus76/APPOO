65d054bad379533026787a4d01af1a1fe7e287f5
Pocus <pocus76000@gmail.com>
2018-03-14 09:16:18 +0100

    chaud

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 ini_set('smtp_port', 1025);
 include "./functions/classAutoLoader.php";
 spl_autoload_register('classAutoLoader');
+
 ?>
 <!DOCTYPE html>
 <html lang="fr-FR">
@@ -15,20 +16,9 @@ spl_autoload_register('classAutoLoader');
 
 <?php
 include "./includes/header.php";
-
-$page = $_GET['page'] ?? "";
-$page = "./includes/" . $page . ".inc.php";
-
-$files = glob("./includes/*.inc.php");
-
-if (in_array($page, $files))
-    include $page;
-else
-    include "./includes/home.inc.php";
-
-
-
+CallPage::display();
 include "./includes/footer.php";
+
 ?>
 </div>
 </body>
