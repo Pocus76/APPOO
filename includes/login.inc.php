0175ffef497607d2bd9694394e18e5f02b5a190d
Pocus <pocus76000@gmail.com>
2018-03-14 14:50:23 +0100

    chaud

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,1 +1,11 @@
 <h1>Login</h1>
+<form action="/actLogin" method="post">
+    <div>
+        <label for="login">Login :</label>
+        <input type="text" id="login" name="login">
+    </div>
+    <div>
+        <label for="pwd">Mot de passe :</label>
+        <input type="password" id="pwd" name="pwd">
+    <div>
+</form>
